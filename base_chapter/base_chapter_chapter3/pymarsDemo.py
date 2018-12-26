8921015c3348b1f967f973471c5beee1242aac2b
BigBigRadish <luozhukun@163.com>
2018-12-26 20:36:03 +0800

    mars内存不会出现溢出

--- base_chapter/base_chapter_chapter3/pymarsDemo.py
+++ base_chapter/base_chapter_chapter3/pymarsDemo.py
@@ -9,11 +9,23 @@ Jiangxi university of finance and economics
 numpy 处理数据
 '''
 import numpy as np
-a=np.random.rand(1000,2000)
-print((a+1).sum(axis=1))
+import time
+def out_wrapper(func):
+    def inner_wrapper():
+        start_time=time.time()
+        func()
+        stop_time=time.time()
+        print('used time {}'.format(stop_time-start_time))
+    return inner_wrapper()
+# @out_wrapper
+# def test1():#10w数据内存溢出
+#     a=np.random.rand(100000,200000)
+#     (a+1).sum(axis=1)
 '''
 pamars多核并行 处理数据
 '''
-import mars.tensor as mt
-a=mt.random.rand(1000,2000)
-print((a+1).sum(axis=1).execute())
+@out_wrapper
+def test2():#10w数据内存没有溢出
+    import mars.tensor as mt
+    a=mt.random.rand(100000,200000)
+    (a+1).sum(axis=1).execute()
